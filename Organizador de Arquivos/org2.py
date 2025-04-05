7dde2c4a3cd97f379fc168f0b3eaf07abae8fe14
Jonas <lopesjonasdaniel@gmail.com>
2025-04-05 18:26:48 -0300

    Creating a new file

--- Organizador de Arquivos/org2.py
+++ Organizador de Arquivos/org2.py
@@ -1,32 +1,41 @@
 import os
 import shutil
-pasta_organizar = input("Digite o caminho da pasta que deseja organizar: ")
 
+pasta_organizar = input("Digite o caminho da pasta que deseja organizar: ")
 
 def organizar_arquivos():
     global pasta_organizar
     pasta_imagens = os.path.join(pasta_organizar, "Imagens")
     pasta_arquivos = os.path.join(pasta_organizar, "Arquivos")
     pasta_documentos = os.path.join(pasta_organizar, "Documentos")
+    os.mkdir(pasta_imagens)
+    os.mkdir(pasta_arquivos)
+    os.mkdir(pasta_documentos)
 
     if not os.path.exists(pasta_organizar):
-        print("Essa pasta não foi encontrada, tente novamente.")
+        print(f"Pasta '{pasta_organizar}' não encontrada.")
+        return
+    if not os.path.isdir(pasta_organizar):
+        print(f"Pasta '{pasta_organizar}' não é um diretório.")
         return
 
     for raiz, pastas, arquivos in os.walk(pasta_organizar):
         for pasta in pastas:
             caminho_pasta = os.path.join(raiz, pasta)
-            shutil.move(caminho_pasta, pasta_organizar)
+            if caminho_pasta != pasta_organizar:
+                shutil.move(caminho_pasta, pasta_organizar)
 
     for raiz, pastas, arquivos in os.walk(pasta_organizar):
         for arquivo in arquivos:
-            caminho_arquivo = os.path.join(raiz, arquivo)
             extensao = os.path.splitext(arquivo)[1]
             if extensao.lower() in [".jpg", ".jpeg", ".png", ".gif", ".bmp"]:
-                shutil.move(caminho_arquivo, pasta_imagens)
+                shutil.move(pasta_imagens)
             elif extensao.lower() in [".py", ".js", ".html", ".css", ".c++" ]:
-                shutil.move(caminho_arquivo, pasta_arquivos)
+                shutil.move(pasta_arquivos)
             else:
-                shutil.move(caminho_arquivo, pasta_documentos)
-
-print(f"O seus arquivos foram organizados, confira na pasta {pasta_organizar}, para ver as mudanças!")
+                shutil.move(pasta_documentos)
+try:
+    organizar_arquivos()
+    print(f"Os arquivos foram organizados com sucesso!")
+except Exception as e:
+    print(f"Ocorreu um erro ao organizar os arquivos: {e}")
