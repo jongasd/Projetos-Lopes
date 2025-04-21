f295ab4420c3be22f01b1a1dbd8b30e0b6928e26
Jonas <lopesjonasdaniel@gmail.com>
2025-04-21 11:53:28 -0300

    Update enviador

--- Enviador de emails/enviador.py
+++ Enviador de emails/enviador.py
@@ -1,2 +1,23 @@
 import smtplib
 import email.message
+destinatario = input("Digite o email do destinatario: ")
+assunto = input("Digite o assunto do email: ")
+corpo = input("Digite o corpo do email: ")
+def enviar_email(destinatario, assunto, corpo):
+    msg = email.message.Message()
+    msg['Subject'] = assunto
+    msg['From'] = 'SEU_EMAIL'
+    msg['To'] = destinatario
+    msg.set_content(corpo)
+
+    smtp = smtplib.SMTP('smtp.gmail.com', 587)
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.login('SEU_EMAIL', 'SUA_SENHA')
+    smtp.send_message(msg)
+    smtp.quit()
+
+    print('Email enviado com sucesso para', destinatario)
+
+# Exemplo de uso
+enviar_email('iMh9B@example.com', 'Assunto do Email', 'Corpo do Email')
