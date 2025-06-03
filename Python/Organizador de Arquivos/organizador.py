3279274725e329e78e11a43d4eaa96aae135002d
Jonas <lopesjonasdaniel@gmail.com>
2025-06-02 21:45:00 -0300

    Update organizador.py

--- Python/Organizador de Arquivos/organizador.py
+++ Python/Organizador de Arquivos/organizador.py
@@ -8,6 +8,7 @@ def organizar_arquivos():
     pasta_imagens = os.path.join(pasta_organizar, "Imagens")
     pasta_arquivos = os.path.join(pasta_organizar, "Arquivos")
     pasta_documentos = os.path.join(pasta_organizar, "Documentos")
+    pasta_outros = os.path.join(pasta_organizar, "Outros")
 
     if not os.path.exists(pasta_organizar):
         print("Essa pasta não foi encontrada, tente novamente.")
@@ -24,9 +25,11 @@ def organizar_arquivos():
             extensao = os.path.splitext(arquivo)[1]
             if extensao.lower() in [".jpg", ".jpeg", ".png", ".gif", ".bmp"]:
                 shutil.move(caminho_arquivo, pasta_imagens)
-            elif extensao.lower() in [".py", ".js", ".html", ".css", ".c++" ]:
+            elif extensao.lower() in [".py", ".js", ".html", ".css", ".c++", ".exec", ".bat"]:
                 shutil.move(caminho_arquivo, pasta_arquivos)
-            else:
+            elif extensao.lower() in [".pdf", ".doc", ".docx", ".xls", ".xlsx", ".ppt", ".pptx", ".excel"]:
                 shutil.move(caminho_arquivo, pasta_documentos)
+            else:
+                shutil.move(caminho_arquivo, pasta_outros)
 
 print(f"O seus arquivos foram organizados, confira na pasta {pasta_organizar}, para ver as mudanças!")
